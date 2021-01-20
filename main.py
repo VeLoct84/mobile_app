e2378781ce402f654cad1bd6e2c43ba95717a48e
VeLoct84 <unlimited84man@gmail.com>
2021-01-20 12:26:30 +0800

    first upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file('design.kv')
+
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class SignUpScreen(Screen):
+    def add_user(self, user, pwd):
+        print(user, pwd)
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+
+if __name__ == "__main__":
+    MainApp().run()
